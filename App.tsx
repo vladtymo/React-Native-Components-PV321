545ef432e38e4d140cee6d86a226aaf6816986e8
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-03-05 18:53:43 +0200

    Components and alerts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
+import { Platform, StatusBar as StatusBarNative, Image, SafeAreaView, StyleSheet, Text, View, Button, TextInput, InputAccessoryView, Alert, Pressable } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  // methods, props, state, hooks, etc.
+
+  // [name, setterName] = useState(initialValue)
+  const [darkMode, setDarkMode] = useState(false);
+  const [len, setLen] = useState(0);
+
+  const showTitle = () => {
+    Alert.alert('Title', 'Do you want to change photo?', [
+      { text: 'No', style: 'destructive' },
+      { text: 'Maybe', onPress: () => console.log('Maybe pressed!'), style: 'cancel' },
+      { text: 'Yes', onPress: () => { setDarkMode(!darkMode); } },
+    ]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      ...styles.container,
+      backgroundColor: darkMode ? 'lightgray' : 'white',
+    }}>
+      <SafeAreaView style={styles.safe}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+
+        <Pressable onPress={showTitle}>
+          <Image
+            source={require(`./assets/icon.png`)}
+            style={styles.image} />
+        </Pressable>
+
+        <Button title="Click me" onPress={showTitle} />
+
+        <TextInput
+          placeholder="Enter your name"
+          style={styles.input}
+          onChangeText={(text) => setLen(text.length)} />
+
+        <Text>Text Length: {len}</Text>
+
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </View>
   );
 }
@@ -16,5 +52,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  safe: {
+    marginTop: Platform.OS === 'android' ? StatusBarNative.currentHeight : 0,
+  },
+  image: {
+    width: 'auto',
+    height: 200,
+    resizeMode: 'contain',
+    borderRadius: 10,
+    margin: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 6,
+    margin: 6,
+    borderRadius: 5,
+  }
 });
